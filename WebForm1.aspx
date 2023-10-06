96bc1bea1b85983df2cffce13859e67c1488dc8e
sk-py <77550660+sk-py@users.noreply.github.com>
2023-10-06 23:59:50 +0530

    Movie Cards Backend Implemented

---
+++ WebForm1.aspx
@@ -0,0 +1,66 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Backend.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+    <title></title>
+    <link rel="stylesheet" href="style.css" />
+              <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <style type="text/css" media="screen and (max-width:800px)">
+        
+            #movie_poster{
+                overflow-y:hidden;
+            }
+
+            #img-wrapper{
+                width:10rem;
+                height:15rem;
+            }
+
+            #movieContainer {
+                width:99%;
+                height:32rem;
+            display: flex;
+            align-items:center;
+            align-items:center;
+            justify-content:center;
+            flex-direction: row;
+            padding:1rem;
+            overflow-y:scroll;
+            position:relative;
+            }
+            #flBtn{
+                position:absolute;
+                bottom:5px;
+                left:50%;
+                display:block;
+            }
+        </style>
+
+</head>
+<body>
+    <form id="form1" runat="server">
+        
+           
+    <div style="width: 100vw; height: 100vh; margin-top: 0;">
+        <span id="heading">
+            <h4>Now Playing In Cinemas</h4>
+            <asp:HyperLink ID="HyperLink1" runat="server" NavigateUrl="WebForm2.aspx">View all &#10095;&#10095;</asp:HyperLink>
+        </span>
+        <div id="n_movies">
+            <div id="movie_poster">
+                 <div id="movieContainer" runat="server" class="media-mobile">
+                    <!-- Movie cards will be dynamically generated here -->
+                </div>
+                <section style="width:100vw;height:100vh;background-color:white;">
+
+                </section>
+            </div>
+        </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace Backend
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        String cnStr;
+        SqlConnection cn;
+
+        public class Movie
+        {
+            public int m_id;
+            public string m_name;
+            //public string Description { get; set; }
+            public string img_url;
+            // Add other properties as needed
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+                cnStr = @"Server=DELL690\SQLEXPRESS;Database=booking;Integrated Security=True;";
+                cn = new SqlConnection(cnStr);
+                cn.Open();
+
+                // Fetch movie data from the database
+                List<Movie> movies = FetchMovieDataFromDatabase();
+
+                // Loop through the movie data and generate movie cards
+                foreach (Movie movie in movies)
+                {
+                    // Create a new HTML div element for each movie card
+                    HtmlGenericControl movieCardDiv = new HtmlGenericControl("div");
+                    movieCardDiv.Attributes["id"] = "img-wrapper";  
+                    HtmlGenericControl movieCardSpan = new HtmlGenericControl("span");
+                    HtmlGenericControl movieName = new HtmlGenericControl("p");
+                    HtmlGenericControl detailsBtn = new HtmlGenericControl("button");
+                    detailsBtn.Attributes["class"] = "dtbtn";
+                    detailsBtn.Attributes["id"] = "dtbtn";
+                    detailsBtn.Attributes["value"] =  movie.m_id.ToString();
+                    
+                    //movieCardSpan.Attributes["id"] = "card-text";
+
+                    // Create an image element for the movie poster
+                    Image moviePosterImage = new Image();
+                    moviePosterImage.ImageUrl = "~/Images/" + movie.img_url;
+                    movieName.InnerText = movie.m_name.ToString();
+                    movieCardDiv.Controls.Add(moviePosterImage);
+                    movieCardDiv.Controls.Add(movieCardSpan);
+                    detailsBtn.InnerText = "View Details";
+                    // Create labels, buttons, and other elements for movie details
+
+                    // Add the movie card to the container
+                    movieContainer.Controls.Add(movieCardDiv);
+                    movieCardDiv.Controls.Add(movieCardSpan);
+                    movieCardSpan.Controls.Add(movieName);
+                    movieCardSpan.Controls.Add(detailsBtn);
+                }
+
+                // Close the database connection
+                cn.Close();
+            
+        }
+
+        private List<Movie> FetchMovieDataFromDatabase()
+        {
+            List<Movie> movies = new List<Movie>();
+
+          
+            SqlCommand command = new SqlCommand("SELECT * FROM Movies", cn);
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Movie movie = new Movie();
+                if (!reader.IsDBNull(0))
+                {
+                    movie.m_id = reader.GetInt32(0);
+                }
+                if (!reader.IsDBNull(1))
+                {
+                    movie.m_name = reader.GetString(1);
+                }
+                if (!reader.IsDBNull(2))
+                {
+                    movie.img_url = reader.GetString(2);
+                }
+                movies.Add(movie);
+            }
+
+            reader.Close();
+            return movies;
+        }
+    }
+}
